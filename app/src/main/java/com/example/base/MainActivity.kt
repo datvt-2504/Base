0fd71454b1160bc2ccc09b4a1eca89cf4e57b835
datvt-2504 <vu.tien.dat-b@sun-asterisk.com>
2021-04-19 21:29:57 +0700

    init

---
+++ app/src/main/java/com/example/base/MainActivity.kt
@@ -0,0 +1,94 @@
+package com.example.base
+
+import android.content.Context
+import android.content.Intent
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+
+class MainActivity : BaseActivity(R.layout.activity_main) {
+
+    private var isBack = false
+
+    private val fragmentManager by lazy {
+        supportFragmentManager
+    }
+
+    var onBackPressedListener: OnBackPressedListener? = null
+
+    override fun onCreatedView() {
+
+        fragmentManager.addOnBackStackChangedListener {
+            if (isBack) {
+                val backStackEntryCount: Int = fragmentManager.backStackEntryCount
+                if (backStackEntryCount > 0 && backStackEntryCount <= fragmentManager.fragments.size) {
+                    try {
+                        val fragment: Fragment = fragmentManager.fragments[backStackEntryCount - 1]
+                        fragment.onResume()
+                    } catch (e: Exception) {
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        if (onBackPressedListener != null) {
+            onBackPressedListener?.doBack()
+        }
+    }
+
+    fun addFragment(fragment: Fragment, tag: String? = "") {
+
+        fragmentManager.beginTransaction()
+                .add(R.id.frameMain, fragment, tag)
+                .addToBackStack(tag).commit()
+
+        val backStackEntryCount: Int = fragmentManager.backStackEntryCount
+        if (backStackEntryCount > 0) {
+            val fragmentStop: Fragment = fragmentManager.fragments[backStackEntryCount - 1]
+            fragmentStop.onStop()
+            fragmentStop.onDestroy()
+            fragmentStop.onDetach()
+        }
+    }
+
+    fun replaceFragment(fragment: Fragment, tag: String? = "", isBackStack: Boolean = false) {
+        if (isBackStack) {
+            fragmentManager.beginTransaction()
+                    .replace(R.id.frameMain, fragment, tag)
+                    .addToBackStack(tag).commit()
+        } else {
+            fragmentManager.beginTransaction()
+                    .replace(R.id.frameMain, fragment, tag).commit()
+        }
+
+        val backStackEntryCount: Int = fragmentManager.backStackEntryCount
+        if (backStackEntryCount > 0) {
+            val fragmentStop: Fragment = fragmentManager.fragments[backStackEntryCount - 1]
+            fragmentStop.onStop()
+            fragmentStop.onDestroy()
+            fragmentStop.onDetach()
+        }
+    }
+
+    fun backFragment(tag: String? = "") {
+        if (fragmentManager.backStackEntryCount > 1) {
+            isBack = true
+            if (tag.isNullOrEmpty()) {
+                fragmentManager.popBackStack()
+            } else {
+                fragmentManager.popBackStack(tag, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+            }
+        } else {
+            finish()
+        }
+    }
+
+    interface OnBackPressedListener {
+        fun doBack()
+    }
+
+    companion object {
+        fun getIntent(context: Context): Intent = Intent(context, MainActivity::class.java)
+    }
+}
